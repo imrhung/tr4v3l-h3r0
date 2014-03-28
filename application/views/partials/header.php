3b0ecd6ee348f23d479633b18d590861854b14fc
nguyenvanhungbkit@gmail.com <nguyenvanhungbkit@gmail.com@be6bd59e-f059-ad58-a0b3-9411a73c8534>
2014-03-28 07:11:15 +0000

    Make Create button in Organization.

--- application/views/partials/header.php
+++ application/views/partials/header.php
@@ -17,9 +17,9 @@
                     <hr class="hr-light-blue">
                 
                 <!-- Create button -->
-                <li class="<?php echo ($current_section == 'quiz') ? 'active' : '' ?>"><a href="<?php echo site_url('organization/create_quiz') ?>"><i class="fa fa-question-circle"></i>  Create Quiz</a></li>
-                <li class="<?php echo ($current_section == 'activity') ? 'active' : '' ?>"><a href="<?php echo site_url('organization/create_activity') ?>"><i class="fa fa-calendar"></i>  Create Activity</a></li>
-                <li class="<?php echo ($current_section == 'donation') ? 'active' : '' ?>"><a href="<?php echo site_url('organization/create_donation') ?>"><i class="fa fa-gift"></i>  Create Donation</a></li>
+                <li class="<?php echo ($current_section == 'quiz') ? 'active' : '' ?> list-background" id="list-background-top"><a href="<?php echo site_url('organization/create_quiz') ?>"><i class="fa fa-question-circle"></i>  Create Quiz</a></li>
+                <li class="<?php echo ($current_section == 'activity') ? 'active' : '' ?> list-background"><a href="<?php echo site_url('organization/create_activity') ?>"><i class="fa fa-calendar"></i>  Create Activity</a></li>
+                <li class="<?php echo ($current_section == 'donation') ? 'active' : '' ?> list-background" id="list-background-bottom"><a href="<?php echo site_url('organization/create_donation') ?>"><i class="fa fa-gift"></i>  Create Donation</a></li>
                 
                 <!-- Management button -->
                 <li class="<?php echo ($current_section == 'home') ? 'active' : '' ?>"><a id="base-url" href="<?php echo site_url('') ?>"><i class="fa fa-home"></i>  Home</a></li>
