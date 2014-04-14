46eb61714d2e81bf618432532f998a3b298b5b3b
nguyenvanhungbkit@gmail.com <nguyenvanhungbkit@gmail.com@be6bd59e-f059-ad58-a0b3-9411a73c8534>
2014-04-14 11:46:26 +0000

    Upload file by AJAX in packet.js (Just upload file to same folder of code, not remote server. Not resize image yet.)

--- application/controllers/packet.php
+++ application/controllers/packet.php
@@ -1,11 +1,12 @@
 <?php
-			/* Tan - Last 13-March-2014*/
-				
+
+/* Tan - Last 13-March-2014 */
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Packet extends App_Controller{
+class Packet extends App_Controller {
 
-	 function __construct() {
+    function __construct() {
         parent::__construct();
         $this->load->library('ion_auth');
         $this->load->library('form_validation');
@@ -18,66 +19,68 @@ class Packet extends App_Controller{
                         $this->load->library('mongo_db') :
                         $this->load->database();
 
-        $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'),
-													$this->config->item('error_end_delimiter', 'ion_auth'));
+        $this->form_validation->set_error_delimiters($this->config->item('error_start_delimiter', 'ion_auth'), $this->config->item('error_end_delimiter', 'ion_auth'));
 
         $this->lang->load('auth');
         $this->load->helper('language');
     }
-	
-					  /*****SELECT*****/
-					/* Last 18-March-2014 */
-					
-	/* Get Packet list function from database*/
-	public function getPacketList(){
-		$result = array();
+
+    /*     * ***SELECT**** */
+    /* Last 18-March-2014 */
+
+    /* Get Packet list function from database */
+
+    public function getPacketList() {
+        $result = array();
         $result['code'] = -1;
         $result['message'] = "";
-		$result['info'] = array();
-		
-		$result['info']['packet'] = "";
-			
+        $result['info'] = array();
+
+        $result['info']['packet'] = "";
+
         $currentPage = $_POST['pageNumber'];
         $pageSize = $_POST['pageSize'];
 
         $resultCheck = $this->packet_model->getPacketList($currentPage, $pageSize);
-		
+
         if ($resultCheck) {
             $result['code'] = 1;
             $result['message'] = "Success";
-            $result['info']['packet'] = $resultCheck ;
+            $result['info']['packet'] = $resultCheck;
         } else {
             $result['code'] = 0;
-            $result['message'] = "Fail";			
-            $result['info']['packet'] = $resultCheck ;
+            $result['message'] = "Fail";
+            $result['info']['packet'] = $resultCheck;
         }
         echo json_encode($result);
-	}
-	
-						/*****INSERT*****/
-					/* Last 18-March-2014 */
-	/*	Packet insert function*/
-	public function insertPacket(){
-		$title 		 = $this->input->post('title');
-		$imageUrl	 = $this->input->post('image_url'); 
-		$partner_id  = $this->input->post('partner_id');		
-		
-		// Initialization Array
+    }
+
+    /*     * ***INSERT**** */
+    /* Last 18-March-2014 */
+    /* 	Packet insert function */
+
+    public function insertPacket() {
+        $title = $this->input->post('title');
+        $imageUrl = $this->input->post('image_url');
+        $partner_id = $this->input->post('partner_id');
+
+        // Initialization Array
         $result = array();
         $result['code'] = -1;
         $result['message'] = "";
-		
-		// Implements work
-		$resultCheck = $this->packet_model->insertPacket($title, $imageUrl, $partner_id);
-		
-		if ($resultCheck) {
+
+        // Implements work
+        $resultCheck = $this->packet_model->insertPacket($title, $imageUrl, $partner_id);
+
+        if ($resultCheck) {
             $result['code'] = 1;
             $result['message'] = "Success";
-        } else{
+        } else {
             $result['code'] = 0;
             $result['message'] = "Fail";
         }
         echo json_encode($result);
-	}
+    }
 }
+
 ?>
--- application/controllers/process.php
+++ application/controllers/process.php
@@ -6,82 +6,124 @@ class Process extends App_Controller {
 
     public function __construct() {
         parent::__construct();
-		
+        $this->load->helper(array('form', 'url'));
+    }
+    
+    public function upload(){
+        // Codeigniter config
+        $config['upload_path'] = './assets/uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size']	= '1000';
+        $config['max_width']  = '1024';
+        $config['max_height']  = '768';
+
+        //set filename in config for upload
+        $config['file_name'] = md5(time());
+        
+        $this->load->library('upload', $config);
+        $this->upload->initialize($config);
+        
+        // Init result json file:
+        $result = array();
+        $result['code'] = -1;
+        $result['message'] = "";
+        $result['info'] = array();
+        
+        if ($this->upload->do_upload()){
+            
+            // Upload successful
+            $result['code'] = 1;
+            $result['message'] = "Success";
+            $result['info'] = array(
+                'file_name' => $this->upload->file_name
+            );
+        } else {
+            // Upload error
+            $result['code'] = 0;
+            $result['message'] = "Fail";
+            echo $this->upload->display_errors();
+        }
+        
+        echo json_encode($result);
     }
 
     public function upload_image() {
-		$this->current_section = 'upload_image';
-		//$this->assets_js[] = '';
-		$this->assets_js[] = 'upload_images/ajax_upload.js';
-		$this->assets_css[] = 'admin.css';
-		$this->render_page_admin('test_upload');
-	}
-	
-	public function uploadfunction() {
-				
-		error_reporting(E_ALL);
-		include('Upload.php');
-		
-		// retrieve eventual CLI parameters
-		$cli = (isset($argc) && $argc > 1);
-		if ($cli) {
-			if (isset($argv[1])) $_GET['file'] = $argv[1];
-			if (isset($argv[2])) $_GET['dir'] = $argv[2];
-			if (isset($argv[3])) $_GET['pics'] = $argv[3];
-		}
+        $this->current_section = 'upload_image';
+        //$this->assets_js[] = '';
+        $this->assets_js[] = 'upload_images/ajax_upload.js';
+        $this->assets_css[] = 'admin.css';
+        $this->render_page_admin('test_upload');
+    }
 
-		// set variables
-		$dir_dest = (isset($_GET['dir']) ? $_GET['dir'] : 'test');
-		$dir_pics = (isset($_GET['pics']) ? $_GET['pics'] : $dir_dest);
+    public function uploadfunction() {
 
-		// we have three forms on the test page, so we redirect accordingly
-		if ((isset($_POST['action']) ? $_POST['action'] : (isset($_GET['action']) ? $_GET['action'] : '')) == 'simple') {
-			$handle = new Upload($_FILES['my_field']);
+        error_reporting(E_ALL);
+        include('Upload.php');
 
-			if ($handle->uploaded) {
-				$handle->image_resize = true;
-				$handle->image_ratio_y = true;
-				$handle->image_x = 50;
-				
-				$handle->Process($dir_dest);
+        // retrieve eventual CLI parameters
+        $cli = (isset($argc) && $argc > 1);
+        if ($cli) {
+            if (isset($argv[1]))
+                $_GET['file'] = $argv[1];
+            if (isset($argv[2]))
+                $_GET['dir'] = $argv[2];
+            if (isset($argv[3]))
+                $_GET['pics'] = $argv[3];
+        }
 
-				// we check if everything went OK
-				if ($handle->processed) {
-					$url = $dir_pics . '/' . $handle->file_dst_name;
-					
-					//$this->load->library('s3_config');
-					$bucket="mytempbucket";
-					if (!class_exists('S3'))
-						require_once('S3.php');
+        // set variables
+        $dir_dest = (isset($_GET['dir']) ? $_GET['dir'] : 'test');
+        $dir_pics = (isset($_GET['pics']) ? $_GET['pics'] : $dir_dest);
 
-					//AWS access info
-					if (!defined('awsAccessKey')) define('awsAccessKey', 'AKIAJIWOJ6L32GWAUUUQ');
-					if (!defined('awsSecretKey')) define('awsSecretKey', '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');
+        // we have three forms on the test page, so we redirect accordingly
+        if ((isset($_POST['action']) ? $_POST['action'] : (isset($_GET['action']) ? $_GET['action'] : '')) == 'simple') {
+            $handle = new Upload($_FILES['my_field']);
 
-					$s3 = new S3(awsAccessKey, awsSecretKey);
-					$s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
-					
-					//Rename image name. 
-					$actual_image_name = time().".".$ext;
-					if($s3->putObjectFile($url, $bucket , $actual_image_name, S3::ACL_PUBLIC_READ)) {
-						$s3file='http://'.$bucket.'.s3.amazonaws.com/'.$actual_image_name;
-						echo "<p style='color: blue'>Image upload successfull.</p><img id='imgPreview' src='" . $s3file . "'/>";
-					} else
-						echo '<p style="color: red">S3 Upload Fail.</p>';
-				} else {
-					// one error occured
-					echo '  Error: ' . $handle->error . '';
-				}
+            if ($handle->uploaded) {
+                $handle->image_resize = true;
+                $handle->image_ratio_y = true;
+                $handle->image_x = 50;
 
-				// we delete the temporary files
-				$handle-> Clean();
-				
-				
-			} else {
-				// if we're here, the upload file failed for some reasons
-				// i.e. the server didn't receive the file
-				echo '  Error: ' . $handle->error . '';
-			}
-		}
-	}
+                $handle->Process($dir_dest);
+
+                // we check if everything went OK
+                if ($handle->processed) {
+                    $url = $dir_pics . '/' . $handle->file_dst_name;
+
+                    //$this->load->library('s3_config');
+                    $bucket = "mytempbucket";
+                    if (!class_exists('S3'))
+                        require_once('S3.php');
+
+                    //AWS access info
+                    if (!defined('awsAccessKey'))
+                        define('awsAccessKey', 'AKIAJIWOJ6L32GWAUUUQ');
+                    if (!defined('awsSecretKey'))
+                        define('awsSecretKey', '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');
+
+                    $s3 = new S3(awsAccessKey, awsSecretKey);
+                    $s3->putBucket($bucket, S3::ACL_PUBLIC_READ);
+
+                    //Rename image name. 
+                    $actual_image_name = time() . "." . $ext;
+                    if ($s3->putObjectFile($url, $bucket, $actual_image_name, S3::ACL_PUBLIC_READ)) {
+                        $s3file = 'http://' . $bucket . '.s3.amazonaws.com/' . $actual_image_name;
+                        echo "<p style='color: blue'>Image upload successfull.</p><img id='imgPreview' src='" . $s3file . "'/>";
+                    } else
+                        echo '<p style="color: red">S3 Upload Fail.</p>';
+                } else {
+                    // one error occured
+                    echo '  Error: ' . $handle->error . '';
+                }
+
+                // we delete the temporary files
+                $handle->Clean();
+            } else {
+                // if we're here, the upload file failed for some reasons
+                // i.e. the server didn't receive the file
+                echo '  Error: ' . $handle->error . '';
+            }
+        }
+    }
+
 }
