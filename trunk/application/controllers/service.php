f880bcc72a152441f4106cf02b40d6a7f4ffe87a
nguyenvanhungbkit@gmail.com <nguyenvanhungbkit@gmail.com@be6bd59e-f059-ad58-a0b3-9411a73c8534>
2014-05-05 06:53:23 +0000

    Add Hung's service to main service file: service.php and service_model.php.

--- trunk/application/controllers/service.php
+++ trunk/application/controllers/service.php
@@ -62,7 +62,7 @@ class Service extends App_Controller {
 		
 		echo json_encode($result);
 	}
-	
+	/*
 	public function getQuizz() {
 		// Initialization Array
         $result = array();
@@ -86,6 +86,8 @@ class Service extends App_Controller {
 		
 		echo json_encode($result);
 	}
+         * 
+         */
 	
 	public function getUserProfile() {
 		// Initialization Array
@@ -110,7 +112,7 @@ class Service extends App_Controller {
 		
 		echo json_encode($result);
 	}
-	
+	/*
 	public function getLeaderBoard() {
 		// Initialization Array
         $result = array();
@@ -134,6 +136,8 @@ class Service extends App_Controller {
 		
 		echo json_encode($result);
 	}
+         * 
+         */
 	
 	public function getUserAwards() {
 		// Initialization Array
@@ -275,4 +279,131 @@ class Service extends App_Controller {
 	public function spentPointActivity() {
 		
 	}
+        
+        /*
+     * Hung's Services:
+     */
+    public function getQuizz() {
+        // Get request params:
+        $pageNumber = $this->input->post('page_number');
+        $pageSize = $this->input->post('page_size');
+        $questId = $this->input->post('quest_id');
+        $random = $this->input->post('random');
+        
+        // Initialization Array
+        $result = array();
+        $result['code'] = -1;
+        $result['message'] = "";
+
+        // Get Animation first:
+        $virtualQuest = $this->service_model_2->getVirtualQuestTable($questId);
+        $animationId = $virtualQuest->AnimationId;
+        $animationRaw = $this->service_model_2->getAnimation($animationId);
+        // Create elements like json defined.
+        $animation = array(
+            'id' => $animationRaw->Id,
+            'time' => $animationRaw->time,
+            'hero_anim_walking' => $animationRaw->HeroAnimWalking,
+            'hero_anim_standby' => $animationRaw->HeroAnimStandby,
+            'monster_anim' => $animationRaw->MonsterAnim,
+            'kid_frame' => $animationRaw->KidFrame,
+            'color_R' => $animationRaw->ColorR,
+            'color_G' => $animationRaw->ColorG,
+            'color_B' => $animationRaw->ColorB
+        );
+        
+        // Then get the quiz list:
+        $category = $this->service_model_2->getQuizCategoryInQuest($questId);
+        $quizList = $this->service_model_2->getQuizChoiceList($pageNumber, $pageSize, $category);
+
+        if ($quizList) {
+            $result['code'] = 1;
+            $result['message'] = "Success";
+            $result['animation'] = $animation;
+            $result['quizzes'] = $quizList;
+        } else {
+            $result['code'] = 0;
+            $result['message'] = "No result";
+        }
+
+        echo json_encode($result);
+    }
+
+    public function getLeaderBoard() {
+        
+        // Get request params:
+        $pageNumber = $this->input->post('page_number');
+        $pageSize = $this->input->post('page_size');
+        $jsonFriends = json_decode($this->input->post('friends_fbid'), TRUE);
+        
+        // Initialization Array
+        $result = array();
+        $result['code'] = -1;
+        $result['message'] = "";
+        
+        // Generate the facebook id list:
+        if (count($jsonFriends['friends_fbid']) > 0){
+            $fbidString = '"0"';
+            for ($i=0; $i<count($jsonFriends['friends_fbid']); $i++){
+                $fbidString .= ',"'. $jsonFriends['friends_fbid'][$i].'"';
+            }
+        } else {
+            $fbidString = null;
+        }
+
+        // Get data
+        $resultCheck = $this->service_model_2->getLeaderBoard($pageNumber, $pageSize, $fbidString);
+        
+        // Because number returned from database is in String, so we need to convert it to integer:
+        $leaderboard = array();
+        foreach ($resultCheck as $row){
+            $leaderboard[] = array(
+                'id' => (int) $row->id,
+                'name' => $row->name,
+                'mark' => (int) $row->mark,
+                'facebook_id' => $row->facebook_id,
+                'current_level' => (int) $row->current_level
+            );
+        }
+        
+        if ($resultCheck) {
+            $result['code'] = 1;
+            $result['message'] = "Success";
+            $result['leaderboard'] = $leaderboard;
+        } else {
+            $result['code'] = 0;
+            $result['message'] = "Fail";
+        }
+        echo json_encode($result);
+    }
+    
+    public function getUserMedal() {
+        
+        // Get request params:
+        $pageNumber = $this->input->post('page_number');
+        $pageSize = $this->input->post('page_size');
+        $userId = $this->input->post('user_id');
+        
+        // Initialization Array
+        $result = array();
+        $result['code'] = -1;
+        $result['message'] = "";
+
+        // Get data
+        $resultCheck = $this->service_model_2->getUserMedal($pageNumber, $pageSize, $userId);
+        
+        if ($resultCheck) {
+            $result['code'] = 1;
+            $result['message'] = "Success";
+            $result['user_medal'] = $resultCheck;
+        } else {
+            $result['code'] = 0;
+            $result['message'] = "Fail";
+        }
+        echo json_encode($result);
+    }
+    
+    /*
+     * End of Hung's code
+     */
 }
