a29a733ce69f8bd9b34980ac0592aa7d53601745
hauquangtruong <hauquangtruong@be6bd59e-f059-ad58-a0b3-9411a73c8534>
2014-05-30 07:53:58 +0000

    Edit function getPackets, add file sp_getPacketsBy.sql.

---
+++ trunk/application/sql/sp_getPacketsBy.sql
@@ -0,0 +1,39 @@
+DROP PROCEDURE IF EXISTS `sp_getPacketsBy`;
+DELIMITER ;;
+CREATE PROCEDURE `sp_getPacketsBy`(IN currentPage INT, IN iPageSize INT)
+BEGIN
+	DECLARE rowNumber INT;
+	SET @currentPage = currentPage;
+	SET @rowNumber = currentPage * iPageSize;
+	SET @pageSize = iPageSize;
+	PREPARE STMT FROM
+		"SELECT  
+			p.Id AS pId,
+			p.Title AS pTitle,
+			p.ImageURL AS pImageURL,
+			p.PartnerId AS pPartnerId,
+			v.Id AS vId,
+			v.QuestName AS vQuestName,
+			v.PacketId AS vPacketId,
+			v.QuestName AS vQuestName,
+			v.PacketId AS vPacketId,
+			v.PartnerId AS vPartnerId,
+			v.AnimationId AS vAnimationId,
+			v.UnlockPoint AS vUnlockPoint,
+			v.CreateDate AS vCreateDate,
+                        v.ImageURL AS questImageUrl,
+			c.Id AS cId,
+			c.Type AS cType,
+			c.Value AS cValue,
+			c.VirtualQuestId AS cVirtualQuestId,
+			c.ObjectId AS cObjectId
+		FROM packet p
+		JOIN (SELECT Id FROM packet LIMIT ?, ?) ids ON p.Id = ids.Id
+		INNER JOIN virtualquest v 
+		ON p.Id = v.PacketId
+		INNER JOIN questcondition c
+		ON c.VirtualQuestId = v.Id
+		ORDER BY pId, vId, cId ASC";
+		EXECUTE STMT USING @rowNumber, @pageSize;
+		DEALLOCATE PREPARE STMT;
+END ;;
